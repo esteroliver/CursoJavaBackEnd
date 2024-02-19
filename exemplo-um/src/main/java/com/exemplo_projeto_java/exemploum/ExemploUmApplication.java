7d130703d0c73e3b6ee980dcb56d76b4bc754d92
Ester Oliveira <oliver.ester.2004@gmail.com>
2024-02-19 19:12:09 -0300

    primeiro projeto springboot

---
+++ exemplo-um/src/main/java/com/exemplo_projeto_java/exemploum/ExemploUmApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo_projeto_java.exemploum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploUmApplication.class, args);
+	}
+
+}
---
+++ exemplo-um/src/test/java/com/exemplo_projeto_java/exemploum/ExemploUmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo_projeto_java.exemploum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
